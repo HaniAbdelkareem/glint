ccdb665bdc4a5377bec322962babad02f2a85095
Hani Abdelkareem <hanim1033@gmail.com>
2024-08-29 00:19:11 +0300

    Setting Convex Auth with Github

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server"
 
-import { ConvexClientProvider } from "@/components/client-convex-provider"
+import { ConvexClientProvider } from "@/components/convex-client-provider"
 
 import "./globals.css"
 
@@ -18,10 +19,12 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
-      </body>
-    </html>
+    <ConvexAuthNextjsServerProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </body>
+      </html>
+    </ConvexAuthNextjsServerProvider>
   )
 }
